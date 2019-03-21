4790814945178b8e22e85815561ac51cf3683abd
tcmulder <tcmulder@gmail.com>
2019-03-21 12:23:37 -0600

    Remove in-dev classes

--- template-parts/module-TEMPLATE.php
+++ template-parts/module-TEMPLATE.php
@@ -3,7 +3,7 @@
      * The template part for displaying the TEMPLATE module.
      */
 ?>
-<section <?php aquamin_module_class('my-class my-other-class'); ?>>
+<section <?php aquamin_module_class( 'TEMPLATE' ); ?>>
     <div class="TEMPLATE__inner mod--inner">
         .TEMPLATE
     </div>
