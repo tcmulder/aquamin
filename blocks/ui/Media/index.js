027de0a4b18bc052e3b85828a59282fcca694b1a
Tomas Mulder <tcmulder@gmail.com>
2022-10-24 16:36:34 -0600

    Overhaul <Media />

--- blocks/ui/Media/index.js
+++ blocks/ui/Media/index.js
@@ -9,21 +9,28 @@
 {
 	// ...other block settings
 	"attributes": {
+		"mediaId": {
+			"type": "number",
+			"default": 0
+		},
 		"mediaAlt": {
-			"type": "string"
+			"type": "string",
+			"source": "attribute",
+			"selector": ".media",
+			"attribute": "alt"
 		},
 		"mediaUrl": {
-			"type": "string"
-		},
-		"mediaId": {
-			"type": "number"
+			"type": "string",
+			"source": "attribute",
+			"selector": ".media",
+			"attribute": "src"
 		},
 		"mediaWidth": {
 			"type": "number"
 		},
 		"mediaHeight": {
 			"type": "number"
-		}
+		},
 	}
 }
 
@@ -49,21 +56,29 @@ There's a lot of customization you can do as well:
 		width: 'mediaWidthBackground',
 		height: 'mediaHeightBackground',
 	},
-	htmlAttributes={{ // extra HTML attributes (videos default to autoplay)
-		video: {
-			loop: '',
-			muted: '',
-			playsinline: '',
+	htmlAttributes={[
+		{
+			// apply html attributes to all media types
+			width: 800,
+			height: 600
 		},
-		image: {
+		{
+			// apply html attribute to image media
+			type: 'image',
 			loading: 'lazy',
 		},
-	}}
+		{
+			// apply html attributes to video media (defaults to autplaying otherwise)
+			type: 'video',
+			controls: '',
+		},
+	]}
 	title={__('Background Media', 'aquamin)}, // title above <MediaPlaceholder />
 	className="my-extra-class" // comes with .media automatically
 	accept={['image/*', 'video/*']} // or exclude a type
 	allowedTypes={['image', 'video']} // or exclude a type
-	style={{ height: 'auto' }} // useful for WP sidebar
+	style={{ height: 'auto' }} // add css style attributes
+	hideInSidebar={true} // don't show this in block editor sidebar
 />
 
  *
@@ -76,7 +91,8 @@ import classnames from 'classnames';
 import { ButtonX } from '../Buttons';
 
 const { __ } = wp.i18n;
-const { MediaPlaceholder } = wp.blockEditor;
+const { MediaPlaceholder, InspectorControls } = wp.blockEditor;
+const { PanelBody, TextareaControl } = wp.components;
 
 /**
  * Get type of media
@@ -95,143 +111,238 @@ const getType = (url) => {
 };
 
 /**
- * Videos and images renderer
+ * Get HTML attributes
  */
-const ActualMedia = ({
+const getHTMLAttributes = (htmlAttributes, type) => {
+	// start with none
+	let allAttributes = {};
+	htmlAttributes?.forEach((attrObj) => {
+		if (attrObj?.type === type) {
+			// if this is the current media type then add html attributes
+			allAttributes = {
+				...allAttributes,
+				...attrObj,
+			};
+		} else if (!attrObj.type) {
+			// if this is for any media type add attributes
+			allAttributes = {
+				...allAttributes,
+				...attrObj,
+			};
+		}
+	});
+	// if this is a video default to autoplay if no attributes given
+	if (type === 'video' && allAttributes?.type !== 'video') {
+		allAttributes = {
+			...allAttributes,
+			loop: '',
+			muted: '',
+			playsinline: '',
+			autoplay: '',
+		};
+	}
+
+	// delete type (if any) since it's not an html attribute
+	delete allAttributes.type;
+
+	return allAttributes;
+};
+
+/**
+ * Output inspector controls
+ */
+const MediaInspector = (props) => {
+	const {
+		title,
+		hideInSidebar,
+		style,
+		editable,
+		attributes,
+		attributeNames,
+		setAttributes,
+	} = props;
+	const show = editable && !hideInSidebar;
+	return show ? (
+		<InspectorControls>
+			<PanelBody title={title}>
+				<MediaEdit {...props} style={{ ...style, height: 'auto' }} />
+				<MediaNew {...props} />
+				{getType(attributes[attributeNames.url]) === 'image' && (
+					<div style={{ marginTop: 10 }}>
+						<TextareaControl
+							label={__('Alt text (alternative text)', 'aquamin')}
+							style={{ marginBottom: -15 }}
+							value={attributes[attributeNames.alt]}
+							onChange={(value) =>
+								setAttributes({
+									[attributeNames.alt]: value,
+								})
+							}
+						/>
+						<p>
+							{__(
+								'Describe the purpose of the image. Leave empty if the image is purely decorative.',
+								'aquamin'
+							)}
+							{` `}
+							<a
+								href="https://www.w3.org/WAI/tutorials/images/decision-tree/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								{__('What is alt text?', 'aquamin')}
+							</a>
+						</p>
+					</div>
+				)}
+			</PanelBody>
+		</InspectorControls>
+	) : null;
+};
+
+/**
+ * Output actual media HTML to save
+ */
+const MediaElement = ({
 	attributeNames,
 	attributes,
 	htmlAttributes,
 	className,
 	style,
+	editable,
 }) => {
-	const type = getType(attributes[attributeNames.url]);
-	// add arbitrary extra html attributes (and default videos to autoplaying)
-	const getHTMLAttributes = () =>
-		// eslint-disable-next-line no-prototype-builtins, no-nested-ternary
-		htmlAttributes?.hasOwnProperty(type)
-			? htmlAttributes[type]
-			: type === 'video'
-			? { loop: '', autoPlay: '', muted: '', playsinline: '' }
-			: null;
-	if (type === 'video' && attributes[attributeNames.url]) {
-		return (
-			// eslint-disable-next-line jsx-a11y/media-has-caption
-			<video
-				className={classnames('media', className)}
-				src={attributes[attributeNames.url]}
-				width={attributes[attributeNames.width]}
-				height={attributes[attributeNames.height]}
-				style={style}
-				{...getHTMLAttributes()}
-			/>
-		);
-	}
-	if (type === 'image' && attributes[attributeNames.url]) {
-		return (
-			<img
-				className={classnames('media', className)}
-				src={attributes[attributeNames.url]}
-				width={attributes[attributeNames.width]}
-				height={attributes[attributeNames.height]}
-				alt={attributes[attributeNames.alt] || __('Media', 'aquamin')}
-				style={style}
-				{...getHTMLAttributes()}
-			/>
-		);
+	const show = !editable;
+	if (show) {
+		const type = getType(attributes[attributeNames.url]);
+		if (type === 'video' && attributes[attributeNames.url]) {
+			return (
+				// eslint-disable-next-line jsx-a11y/media-has-caption
+				<video
+					className={classnames('media', className)}
+					src={attributes[attributeNames.url]}
+					width={attributes[attributeNames.width]}
+					height={attributes[attributeNames.height]}
+					style={style}
+					{...getHTMLAttributes(htmlAttributes, type)}
+				/>
+			);
+		}
+		if (type === 'image' && attributes[attributeNames.url]) {
+			return (
+				<img
+					className={classnames('media', className)}
+					src={attributes[attributeNames.url]}
+					width={attributes[attributeNames.width]}
+					height={attributes[attributeNames.height]}
+					alt={attributes[attributeNames.alt]}
+					style={style}
+					{...getHTMLAttributes(htmlAttributes, type)}
+				/>
+			);
+		}
 	}
 
 	return null;
 };
 
 /**
- * Basic video adder
+ * Output existing media editor
  */
-const Media = ({
-	attributeNames = {
+const MediaEdit = (props) => {
+	const { attributeNames, attributes, setAttributes, editable } = props;
+	const show = editable && attributes[attributeNames.url];
+	return show ? (
+		<>
+			<div className="aquamin-media-remove">
+				<ButtonX
+					label={__('Remove Media', 'lift-the-label')}
+					handleClick={() =>
+						setAttributes({
+							[attributeNames.id]: '',
+							[attributeNames.url]: '',
+							[attributeNames.alt]: '',
+							[attributeNames.width]: '',
+							[attributeNames.height]: '',
+						})
+					}
+					style={{
+						position: 'absolute',
+						zIndex: '10',
+						border: '2px solid currentColor',
+					}}
+				/>
+			</div>
+			<MediaElement {...props} editable={false} />
+		</>
+	) : null;
+};
+
+/**
+ * Output new media adder
+ */
+const MediaNew = ({
+	className,
+	style,
+	title,
+	attributes,
+	attributeNames,
+	setAttributes,
+	accept,
+	allowedTypes,
+	editable,
+}) => {
+	const show = editable && !attributes[attributeNames.url];
+	return show ? (
+		<MediaPlaceholder
+			disableDropZone={false}
+			className={classnames('media', className)}
+			style={style}
+			labels={{
+				title,
+			}}
+			value={attributes[attributeNames.id]}
+			onSelectURL={(value) =>
+				setAttributes({
+					[attributeNames.url]: value,
+				})
+			}
+			onSelect={(value) =>
+				setAttributes({
+					[attributeNames.id]: value.id,
+					[attributeNames.url]: value.url,
+					[attributeNames.alt]: value.alt,
+					[attributeNames.width]: value.width,
+					[attributeNames.height]: value.height,
+				})
+			}
+			accept={accept}
+			allowedTypes={allowedTypes}
+		/>
+	) : null;
+};
+
+/**
+ * Show media component
+ */
+const Media = (props) => (
+	<>
+		<MediaInspector {...props} />
+		<MediaEdit {...props} />
+		<MediaNew {...props} />
+		<MediaElement {...props} />
+	</>
+);
+Media.defaultProps = {
+	title: __('Media Upload', 'aquamin'),
+	accept: ['image/*', 'video/*'],
+	allowedTypes: ['image', 'video'],
+	attributeNames: {
 		alt: 'mediaAlt',
 		url: 'mediaUrl',
 		id: 'mediaId',
 		width: 'mediaWidth',
 		height: 'mediaHeight',
 	},
-	title,
-	setAttributes,
-	attributes,
-	htmlAttributes,
-	className,
-	style,
-	editable,
-	accept,
-	allowedTypes,
-}) => {
-	const willAccept = accept || ['image/*', 'video/*'];
-	const willAllowTypes = allowedTypes || ['image', 'video'];
-	if (editable && attributes[attributeNames.url]) {
-		return (
-			<>
-				<div className="aquamin-media-remove">
-					<ButtonX
-						label={__('Remove Media', 'lift-the-label')}
-						handleClick={() =>
-							setAttributes({
-								[attributeNames.id]: '',
-								[attributeNames.url]: '',
-								[attributeNames.alt]: '',
-								[attributeNames.width]: '',
-								[attributeNames.height]: '',
-							})
-						}
-						style={{ position: 'absolute', zIndex: '10' }}
-					/>
-				</div>
-				<ActualMedia
-					attributeNames={attributeNames}
-					attributes={attributes}
-					className={classnames('media', className)}
-					style={style}
-					htmlAttributes={htmlAttributes}
-				/>
-			</>
-		);
-	}
-	if (editable) {
-		return (
-			<MediaPlaceholder
-				disableDropZone={false}
-				className={classnames('media', className)}
-				style={style}
-				labels={{
-					title: title || __('Media Upload', 'lift-the-label'),
-				}}
-				value={attributes[attributeNames.id]}
-				onSelectURL={(value) =>
-					setAttributes({
-						[attributeNames.url]: value,
-					})
-				}
-				onSelect={(value) =>
-					setAttributes({
-						[attributeNames.id]: value.id,
-						[attributeNames.url]: value.url,
-						[attributeNames.alt]: value.alt,
-						[attributeNames.width]: value.width,
-						[attributeNames.height]: value.height,
-					})
-				}
-				accept={willAccept}
-				allowedTypes={willAllowTypes}
-			/>
-		);
-	}
-
-	return (
-		<ActualMedia
-			attributeNames={attributeNames}
-			attributes={attributes}
-			className={className}
-			htmlAttributes={htmlAttributes}
-		/>
-	);
 };
 
 export default Media;
