837e645bc75d9dfb1a4a6d9e19c1e7f98c72f8fe
Tomas Mulder <tcmulder@gmail.com>
2023-02-08 17:08:24 -0700

    Make file paths more efficient

--- includes/cli/templates/block-dynamic/index.php
+++ includes/cli/templates/block-dynamic/index.php
@@ -5,11 +5,11 @@
 
 // register the block
 register_block_type_from_metadata(
-	dirname( __FILE__ ), [
+	__DIR__, [
 		'render_callback' => function( $attributes, $content, $block ) {
 			ob_start();
 			get_template_part(
-				'blocks/block-library/' . basename( dirname( __FILE__ ) ) . '/markup',
+				'blocks/block-library/' . basename( __DIR__ ) . '/markup',
 				null,
 				[
 					'attributes' => $attributes,
