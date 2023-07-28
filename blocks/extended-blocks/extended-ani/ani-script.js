9844b69ff56d85e0ef4b3e063098b178a01da645
Tomas Mulder <tcmulder@gmail.com>
2023-07-28 10:42:39 -0600

    Refactor and comment ani files

--- blocks/extended-blocks/extended-ani/ani-script.js
+++ blocks/extended-blocks/extended-ani/ani-script.js
@@ -6,17 +6,25 @@
  */
 
 // import animations
-import { aniLoad } from './scripts/aniLoad';
 import { aniScroll } from './scripts/aniScroll';
 import { aniBackground } from './scripts/aniBackground';
-import { ani, aniDelegate } from './scripts/aniViewport';
+import { ani, aniDelegate, aniLoad } from './scripts/aniViewport';
 
-// execute animations
+// animate items once on page load
 aniLoad('.ani--load');
+
+// animate items on scroll (scrubs animation as you scroll)
 aniScroll('.ani--parallax');
+
+// animate background colors and media (like background-attachment:fixed)
 aniBackground('.ani--background');
+
+// animate foreground colors and media (like position:fixed popup)
 aniBackground('.ani--foreground', true);
+
+// animate items when they enter the viewport
 ani([
+	// delegate some animations (i.e. Gutenberg applies classes to parent and we'll move them to an appropriate child)
 	...aniDelegate([
 		{ parent: '.ani-parent', child: '.ani-child' },
 		// eslint-disable-next-line prettier/prettier
@@ -24,5 +32,6 @@ ani([
 		{ parent: '.wp-block-columns', child: '.wp-block-column' },
 		{ parent: '.wp-block-buttons', child: '.wp-block-button' },
 	]),
+	// must come second so aniDelegate can move .ani class to children and off of the parent
 	...document.querySelectorAll('.ani'),
 ]);
--- blocks/extended-blocks/extended-ani/scripts/aniViewport.js
+++ blocks/extended-blocks/extended-ani/scripts/aniViewport.js
@@ -33,8 +33,9 @@ export const ani = (els) => {
  *
  * Apply animation classes to children of
  * elements rather than their parents, with
- * incremental delays. Send the returned elements
- * through ani() to apply the animations.
+ * incremental delays. (You'll then need to
+ * send the returned elements through ani()
+ * to apply the animations.)
  *
  * @param {array} delegatedSelectors Array of parent/child selector pairs.
  * @param {string} aniClassName  Name of basic animations (what ani() will use)
@@ -79,4 +80,52 @@ export const aniDelegate = (delegatedSelectors, aniClassName = 'ani') => {
 		}
 	);
 	return allAniEls;
+};
+
+/**
+ * On-Load Animations
+ *
+ * Run animations once on document ready
+ * and never again.
+ *
+ * @param {mixed} element Selector string, nodelist, or array of nodes.
+ * @param {array} aniClassNameList Class names for animations (so we can remove it)
+ */
+export const aniLoad = (element, aniClassNameList = ['ani']) => {
+	const els =
+		typeof element === 'string'
+			? document.querySelectorAll(element)
+			: element;
+	if (els.length) {
+		els.forEach((el) => {
+			// remove other animation types (like viewport-based animations)
+			el.classList.remove(...aniClassNameList);
+			// if this is a loadable media element
+			if (el.complete !== undefined) {
+				if (el.complete) {
+					// show the element after a bit if it's already cached
+					setTimeout(() => {
+						el.classList.add('is-shown');
+					}, 100);
+				} else {
+					// show the element after it loads for the first time
+					el.addEventListener('load', () => {
+						el.classList.add('is-shown');
+					});
+				}
+			} else if (
+				['interactive', 'complete'].includes(document.readyState)
+			) {
+				// show the element if the page is already ready
+				el.classList.add('is-shown');
+			} else {
+				// show the element on page ready
+				document.addEventListener(
+					'DOMContentLoaded',
+					() => el.classList.add('is-shown'),
+					false
+				);
+			}
+		});
+	}
 };
