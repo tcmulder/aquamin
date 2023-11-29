a2b47501d4700a05b71ef4100d1e3440839f0931
Tomas Mulder <tcmulder@gmail.com>
2023-11-29 12:59:19 -0700

    Remove demo block

--- assets/block-library/my-dynamic-block/my-dynamic-block-script.js
+++
@@ -1,9 +0,0 @@
-/**
- * My Dynamic Block JavaScript
- *
- * This file defines My Dynamic Block
- * behavior.
- */
-
-// eslint-disable-next-line no-console
-console.log('my-dynamic-block script has loaded'); // replace with your own code
