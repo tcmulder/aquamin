33f9b91811292268dfc6487f631341f20468539b
Tomas Mulder <tcmulder@gmail.com>
2024-05-10 07:05:47 -0600

    Rename

--- aquatester.php
+++ aquatester.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Plugin Name: Aquamin Tests
+Plugin Name: Aquamarine Playwright Tests
 Description: E2E test suite for the aquamin theme.
 Version:     1.0.0
 Author:      Tomas Mulder
 Author URI:  https://www.thinkaquamarine.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: aquamin-tests
+Text Domain: aquatester
 */
 
 /**
